35944e1bbc6f9ed9c8bf2a6e504fede59e0674d2
Edrian <razer4832@gmail.com>
2025-05-20 10:19:56 +0300

    It is the Options of enum class that with them we can choose options from menu)

---
+++ Options.py
@@ -0,0 +1,15 @@
+from enum import Enum, auto
+
+
+class Menu_Options(Enum):
+    Save_New_Entry = auto()
+    Search_By_Id = auto()
+    Print_Ages_Avg  = auto()
+    Print_All_Names  = auto()
+    Print_All_Ids = auto()
+    Print_Entry_By_Index = auto()
+    Print_All_Entries_Using_Polymyarizm = auto()
+    Save_All_Data = auto()
+    Exit = auto()
+
+
